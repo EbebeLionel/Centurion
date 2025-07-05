c68ad0be5d22c99f8e8c4ac7932b61cb11d0814f
Ebebe Lionel <ebebelionel25@gmail.com>
2025-07-04 22:22:14 -0400

    Updated Header, Model and Home Page

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
+import Header from './components/Header_space/Header';
+import Home from './components/Home_space/Home';
+import SignUp from './components/SignUp_space/SignUp';
+import Models from './components/Models_space/Models';
+import Login from './components/Login_space/Login';
+import { JSX } from 'react';
 
-function App() {
+function App(): JSX.Element {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Header />} />
+        <Route path="/models" element={<Models />} />
+      </Routes>
+      <Routes>
+        <Route path="/" element={<Header />} />
+        <Route path="/home" element={<Home />} />
+      </Routes>
+      <Routes>
+        <Route path="/" element={<Header />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>    
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/signup" element={<SignUp />} />
+      </Routes>
+    </Router>
   );
 }
 
